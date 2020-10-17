2c769b38061c14edf51c453767a265e70df10a15
[VivekGarg1] <[vivekgarg020@gmail.com]>
2020-10-17 06:42:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/*let name=prompt("What is your name?");
+    document.getElementById('welcome').innerHTML="Welcome "+
+    name;*/
+    console.time('test');
+    console.log(2+2);
+    console.log([1,2,3]);
+    var a="Learning Java Script";
+    console.log(a);
+    console.table([1,2,3]);
+    console.error("wrong");
+    console.timeEnd('test');
+    console.clear();
+
+    
---
+++ light.js
@@ -0,0 +1,11 @@
+function turnLightOn()
+{
+    var image=document.getElementById("myImage");
+    image.src="light-on.gif";
+}
+
+function turnLightOff()
+{
+    var image=document.getElementById("myImage");
+    image.src="light-off.gif";
+}
